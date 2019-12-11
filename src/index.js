19d5c98fb432b7ab24f06fe13032d19821516caf
Ethan Hoover <51389138+Cireimu@users.noreply.github.com>
2019-12-11 13:17:16 -0800

    mvp reached

--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { appReducer } from './reducers/reducers';
+
+// Redux Imports
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+
+
+
 import App from './App';
 
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
+
+
+const store = createStore(appReducer);
+console.log(store)
+
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, 
+rootElement);
